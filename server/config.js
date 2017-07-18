c8d77c0645d749bb0b003f827e350dccd32cf5cd
xrr2016 <xiaoranran1993@163.com>
2017-07-18 23:23:39 +0800

    :fire:

---
+++ server/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  AUTH0_DOMAIN: 'qjkm.auth0.com',
+  AUTH0_API_AUDIENCE: 'http://localhost:8083/api/',
+  MONGO_URI: 'mongodb://xrr2016:xiaoran123@ds161032.mlab.com:61032/mean'
+}
