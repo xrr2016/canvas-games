ac5227e5ef55161fc7c6fa6bfca42cd811edcd45
xrr2016 <xiaoranran1993@163.com>
2017-07-29 22:07:43 +0800

    :art:

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,32 +1,26 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Router, NavigationStart } from '@angular/router';
-import 'rxjs/add/operator/filter'
+import 'rxjs/add/operator/filter';
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+	selector: 'app-header',
+	templateUrl: './header.component.html',
+	styleUrls: [ './header.component.scss' ]
 })
 export class HeaderComponent implements OnInit {
-  @Output() navToggled = new EventEmitter()
-  navOpen = false
-  constructor(private router: Router) { }
+	@Output() navToggled = new EventEmitter();
+	navOpen = false;
 
-  ngOnInit() {
-    this.router.events
-        .filter(event => event instanceof NavigationStart && this.navOpen)
-        .subscribe(event => this.toggleNav)
-  }
+	constructor(private router: Router) {}
 
-  toggleNav() {
-    this.navOpen = !this.navOpen
-    this.navToggled.emit(this.navOpen)
-  }
+	ngOnInit() {
+		this.router.events
+			.filter((event) => event instanceof NavigationStart && this.navOpen)
+			.subscribe((event) => this.toggleNav());
+	}
+
+	toggleNav() {
+		this.navOpen = !this.navOpen;
+		this.navToggled.emit(this.navOpen);
+	}
 }
-
-
-
-
-
-
-
